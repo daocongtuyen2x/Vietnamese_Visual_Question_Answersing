134b562e5e58f7012056ee09f67e459bf31a4cad
daocongtuyen2x <daocongtuyen2x@gmail.com>
2023-03-08 07:41:33 +0700

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+import sys
+
+class CFG:
+    # Path to the ViVQA dataset
+    train_csv_path = 'data/train.csv'
+    test_csv_path = 'data/test.csv'
+    label_dict_path = 'data/label_dict.json'
+    image_dir = 'viq_images'
+
+    

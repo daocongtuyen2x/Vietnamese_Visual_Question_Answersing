6d50aadd638b3817275d93edc0adb8cc4851a10c
daocongtuyen2x <daocongtuyen2x@gmail.com>
2023-03-14 08:09:12 +0700

    add files

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from torchvision import transforms
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+import torch.nn as nn
 
 def get_transforms():
     return Compose([
@@ -10,3 +11,13 @@ def get_transforms():
         ToTensor(),
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+def init_weights(module):
+    if isinstance(module, (nn.Linear, nn.Embedding)):
+        module.weight.data.normal_(mean=0.0, std=0.02)
+    elif isinstance(module, nn.LayerNorm):
+        module.bias.data.zero_()
+        module.weight.data.fill_(1.0)
+
+    if isinstance(module, nn.Linear) and module.bias is not None:
+        module.bias.data.zero_()

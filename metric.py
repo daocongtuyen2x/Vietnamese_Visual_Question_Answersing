759b4b4d741e82fc0ee3fd1f5542fc6c79012f13
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2023-04-10 18:25:36 +0700

    Add files via upload

---
+++ metric.py
@@ -0,0 +1,73 @@
+import sys
+import pickle
+#import enchant
+
+from numpy import prod
+from nltk.corpus import wordnet as wn
+
+with open('translateDict.pkl', 'rb') as f:
+    translateDict = pickle.load(f)
+
+print(translateDict)
+
+def wup_measure(a,b,simiarity_threshold=0.925):
+    """
+    Returns Wu-Palmer similarity score.
+    More specifically, it computes:
+        max_{x \in interp(a)} max_{y \in interp(b)} wup(x,y)
+        where interp is a 'interpretation field'
+    """
+    a = translateDict[a]
+    b = translateDict[b]
+    def get_semantic_field(a):
+        weight = 1.0
+        semantic_field = wn.synsets(a,pos=wn.NOUN)
+        return (semantic_field,weight)
+
+
+    def get_stem_word(a):
+        """
+        Sometimes answer has form word\d+:wordid.
+        If so we return word and downweight
+        """
+        weight = 1.0
+        return (a,weight)
+
+
+    global_weight=1.0
+
+    (a,global_weight_a)=get_stem_word(a)
+    (b,global_weight_b)=get_stem_word(b)
+    global_weight = min(global_weight_a,global_weight_b)
+
+    if a==b:
+        # they are the same
+        return 1.0*global_weight
+
+    if a==[] or b==[]:
+        return 0
+
+    interp_a,weight_a = get_semantic_field(a) 
+    interp_b,weight_b = get_semantic_field(b)
+
+    if interp_a == [] or interp_b == []:
+        return 0
+
+    # we take the most optimistic interpretation
+    global_max=0.0
+    for x in interp_a:
+        for y in interp_b:
+            local_score=x.wup_similarity(y)
+            if local_score > global_max:
+                global_max=local_score
+
+    # we need to use the semantic fields and therefore we downweight
+    # unless the score is high which indicates both are synonyms
+    if global_max < similarity_threshold:
+        interp_weight = 0.1
+    else:
+        interp_weight = 1.0
+
+    final_score=global_max*weight_a*weight_b*interp_weight*global_weight
+    return final_score 
+###
